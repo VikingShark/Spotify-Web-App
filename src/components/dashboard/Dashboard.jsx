9af9ca9eeafb59247ab17b20de67603aabdd611a
Villiam Mejlvang Strannerklint <villiam.strannerklint@hotmail.com>
2025-04-01 15:40:02 +0200

    Added conditional rendering for token with loading state

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -11,6 +11,7 @@ import Library from '../../pages/Library';
 
 const Dashboard = ({ spotifyApi }) => {
     const [token, setToken] = useState(getAccessTokenFromStorage());
+    const [loading, setLoading] = useState(true)
 
     useEffect(() =>{
         async function onMount() {
@@ -24,14 +25,14 @@ const Dashboard = ({ spotifyApi }) => {
     return ( 
         <Box sx={{width: '100%', height: '100%', display: 'flex', flexDirection: 'column'}}>
             <Box sx={{flex: 1, overflowY: 'auto', display: 'flex'}}>
-                <SideNav spotifyApi={spotifyApi} token={token}/>
+                <SideNav spotifyApi={spotifyApi} token={!loading ? token : null}/>
                 <Routes>
-                    <Route path="/playlist/:id"  element={ <Playlist spotifyApi={spotifyApi} token={token}/> }/>
-                    <Route path="/library" element={<Library spotifyApi={spotifyApi} token={token} />} />
+                    <Route path="/playlist/:id"  element={ <Playlist spotifyApi={spotifyApi} token={!loading ? token : null}/> }/>
+                    <Route path="/library" element={<Library spotifyApi={spotifyApi} token={!loading ? token : null} />} />
                     <Route path="/" element={<Home />}/>
                 </Routes>
             </Box>
-            {token && <Player spotifyApi={spotifyApi} token={token} />}
+            {token && <Player spotifyApi={spotifyApi} token={!loading ? token : null} />}
             <MobileNav />
         </Box>
      );
